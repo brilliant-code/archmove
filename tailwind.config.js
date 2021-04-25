c62b70e80371cd54d628e172f3c5ec3b5abd79a0
Mr. CaT <mrcat323@gmail.com>
2021-04-25 18:20:49 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sf-ui-normal': ['SF UI Display Medium'],
+        'sf-ui-light': ['SF UI Display Light'],
+        'new-york': ['New York Extra Large Regular']
+      },
+      colors: {
+        'redish': '#FD665E',
+      }  
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
